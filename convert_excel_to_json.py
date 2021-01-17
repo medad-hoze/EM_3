e41ca8f45322bf373e56d88442d4598d211c7459
meidad hoze <48644044+medad-hoze@users.noreply.github.com>
2021-01-17 13:36:11 +0200

    Add files via upload

--- convert_excel_to_json.py
+++ convert_excel_to_json.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-def read_excel_to_json(path2):
+def read_excel_to_json(path2,out_put):
     # combine sheets to dataframe
     x1 = pd.ExcelFile(path2)
     df = pd.DataFrame()
@@ -15,9 +15,10 @@ def read_excel_to_json(path2):
             print "coudent read sheet {}".format(name)
         df = df.append(sheet,ignore_index = True)
     
-    json = df.to_json(r'C:\Users\medad\python\GIStools\Work Tools\Engine_Cad_To_Gis\Json_try.json')
+    json = df.to_json(out_put)
     return json
 
 
-path2 = r"C:\Users\medad\python\GIStools\Work Tools\Engine_Cad_To_Gis\DATA_DIC_20200218-MAVAAT.xlsx"
+path2   = r"C:\Users\medad\python\GIStools\Work Tools\Engine_Cad_To_Gis\DATA_DIC_20200218-MAVAAT.xlsx"
+out_put = r'C:\Users\medad\python\GIStools\Work Tools\Engine_Cad_To_Gis\Json_try.json'
 read_excel_to_json(path2)
